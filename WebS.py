c851e4d9256504d5f226ce3c142716a46305ec4c
michal <micdeg96@gmail.com>
2021-01-17 16:33:53 +0100

    zmiany

--- WebS.py
+++ WebS.py
@@ -2,17 +2,28 @@
 Autor: Michał Degowski
 """
 
+import threading
 import pprint
 import requests
 import re
 from bs4 import BeautifulSoup
 
-search = input("Search:")
-results = 10
-response = requests.get(f"https://www.google.com/search?q={search}&num={results}")
-soup = BeautifulSoup(response.text, "html.parser")
-result = soup.find_all(attrs = {'class': 'ZINbbc'})
-results = [re.search('\/url\?q\=(.*)\&sa',str(i.find('a')['href'])) for i in result]
-links = [i.group(1) for i in results if i != None]
-pprint.pprint(links)
+print("Search: ")
 
+def WebSearch():
+    search = input()
+    results = 10
+    response = requests.get(f"https://www.google.com/search?q={search}&num={results}")
+    soup = BeautifulSoup(response.text, "html.parser")
+    result = soup.find_all(attrs={'class': 'ZINbbc'})
+    results = [re.search('\/url\?q\=(.*)\&sa', str(i.find('a')['href'])) for i in result]
+    links = [i.group(1) for i in results if i != None]
+    pprint.pprint(links)
+
+
+watek = threading.Thread(target=WebSearch)
+watek.start()
+watek2 = threading.Thread(target=WebSearch)
+watek2.start()
+watek.join()
+watek2.join()
